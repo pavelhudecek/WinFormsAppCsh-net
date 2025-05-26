bfa8ee3a0ae8631ce8dca92559071dfd6cb46056
Pavel Hudecek <pavel.hudecek@advacam.com>
2025-05-26 13:35:12 +0200

    bbb

---
+++ WinFormsAppCsh-net/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WinFormsAppCsh_net
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            buttonSendMsg = new Button();
+            listMessages = new ListBox();
+            textSendMsg = new TextBox();
+            textIPadr = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textPort = new TextBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            buttonUDPstart = new Button();
+            buttonTCPstart = new Button();
+            buttonStop = new Button();
+            SuspendLayout();
+            // 
+            // buttonSendMsg
+            // 
+            buttonSendMsg.Enabled = false;
+            buttonSendMsg.Location = new Point(452, 573);
+            buttonSendMsg.Name = "buttonSendMsg";
+            buttonSendMsg.Size = new Size(126, 71);
+            buttonSendMsg.TabIndex = 0;
+            buttonSendMsg.Text = "buttonSendMsg";
+            buttonSendMsg.UseVisualStyleBackColor = true;
+            buttonSendMsg.Click += buttonSendMsg_Click;
+            // 
+            // listMessages
+            // 
+            listMessages.FormattingEnabled = true;
+            listMessages.ItemHeight = 15;
+            listMessages.Location = new Point(12, 12);
+            listMessages.Name = "listMessages";
+            listMessages.Size = new Size(566, 439);
+            listMessages.TabIndex = 1;
+            // 
+            // textSendMsg
+            // 
+            textSendMsg.Location = new Point(12, 573);
+            textSendMsg.Multiline = true;
+            textSendMsg.Name = "textSendMsg";
+            textSendMsg.Size = new Size(434, 71);
+            textSendMsg.TabIndex = 2;
+            // 
+            // textIPadr
+            // 
+            textIPadr.Location = new Point(57, 457);
+            textIPadr.Name = "textIPadr";
+            textIPadr.Size = new Size(257, 23);
+            textIPadr.TabIndex = 3;
+            textIPadr.Text = "127.0.0.1";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 460);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 15);
+            label1.TabIndex = 4;
+            label1.Text = "IP/adr:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(320, 460);
+            label2.Name = "label2";
+            label2.Size = new Size(32, 15);
+            label2.TabIndex = 5;
+            label2.Text = "port:";
+            // 
+            // textPort
+            // 
+            textPort.Location = new Point(358, 457);
+            textPort.Name = "textPort";
+            textPort.Size = new Size(88, 23);
+            textPort.TabIndex = 6;
+            textPort.Text = "12345";
+            // 
+            // buttonUDPstart
+            // 
+            buttonUDPstart.Location = new Point(12, 486);
+            buttonUDPstart.Name = "buttonUDPstart";
+            buttonUDPstart.Size = new Size(138, 23);
+            buttonUDPstart.TabIndex = 7;
+            buttonUDPstart.Text = "buttonUDPstart";
+            buttonUDPstart.UseVisualStyleBackColor = true;
+            buttonUDPstart.Click += buttonUDPstart_Click;
+            // 
+            // buttonTCPstart
+            // 
+            buttonTCPstart.Location = new Point(156, 486);
+            buttonTCPstart.Name = "buttonTCPstart";
+            buttonTCPstart.Size = new Size(138, 23);
+            buttonTCPstart.TabIndex = 8;
+            buttonTCPstart.Text = "buttonTCPstart";
+            buttonTCPstart.UseVisualStyleBackColor = true;
+            buttonTCPstart.Click += buttonTCPstart_Click;
+            // 
+            // buttonStop
+            // 
+            buttonStop.Location = new Point(336, 486);
+            buttonStop.Name = "buttonStop";
+            buttonStop.Size = new Size(110, 23);
+            buttonStop.TabIndex = 9;
+            buttonStop.Text = "buttonStop";
+            buttonStop.UseVisualStyleBackColor = true;
+            buttonStop.Click += buttonStop_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(584, 742);
+            Controls.Add(buttonStop);
+            Controls.Add(buttonTCPstart);
+            Controls.Add(buttonUDPstart);
+            Controls.Add(textPort);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textIPadr);
+            Controls.Add(textSendMsg);
+            Controls.Add(listMessages);
+            Controls.Add(buttonSendMsg);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonSendMsg;
+        private ListBox listMessages;
+        private TextBox textSendMsg;
+        private TextBox textIPadr;
+        private Label label1;
+        private Label label2;
+        private TextBox textPort;
+        private System.Windows.Forms.Timer timer1;
+        private Button buttonUDPstart;
+        private Button buttonTCPstart;
+        private Button buttonStop;
+    }
+}
---
+++ WinFormsAppCsh-net/Form1.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace WinFormsAppCsh_net
+{
+    public partial class Form1 : Form
+    {
+        private UdpClient _udpClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonUDPstart_Click(object sender, EventArgs e)
+        {
+            // Pokud chcete zároveò IPv4 i IPv6 pøíjem, použijte DualMode
+            _udpClient = new UdpClient(AddressFamily.InterNetworkV6);
+            _udpClient.Client.DualMode = true;   // umožní pøijímat i IPv4
+            _udpClient.Client.Bind(new IPEndPoint(IPAddress.IPv6Any, 12345));
+            
+            udpBeginReceive();
+            buttonUDPstart.Enabled = false;
+            buttonStop.Enabled = true;
+            buttonSendMsg.Enabled = true;
+        }
+
+        private void udpBeginReceive()
+        {
+            // zaregistrujeme callback, který se spustí pøi pøijetí paketu
+            _udpClient.BeginReceive(ReceiveCallback, null);
+        }
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            IPEndPoint remoteEP = null;
+            byte[] buffer;
+
+            try
+            {
+                buffer = _udpClient.EndReceive(ar, ref remoteEP);
+            }
+            catch (ObjectDisposedException)
+            {
+                // Socket už je uzavøený
+                return;
+            }
+            catch (Exception ex)
+            {
+                // Zpracovat chybu, pøípadnì zkusit znovu
+                this.Invoke((Action)(() =>
+                    listMessages.Items.Add($"Chyba: {ex.Message}")
+                ));
+                udpBeginReceive();
+                return;
+            }
+
+            // Dekódujeme zprávu a pøidáme na UI
+            string msg = Encoding.UTF8.GetString(buffer);
+            this.Invoke((Action)(() =>
+                listMessages.Items.Add("Recv: " + msg)
+            ));
+
+            // Zaregistrujeme pøíjem další zprávy
+            udpBeginReceive();
+        }
+
+        private void buttonTCPstart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            _udpClient.Close();
+            buttonUDPstart.Enabled = true;
+            buttonSendMsg.Enabled = false;
+        }
+
+        private void buttonSendMsg_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] data = Encoding.UTF8.GetBytes(textSendMsg.Text);
+                // Adresa a port pøíjemce
+                IPAddress remoteIp = IPAddress.Parse(textIPadr.Text);
+                int remotePort = Convert.ToInt32(textPort.Text);
+
+                var endpoint = new IPEndPoint(remoteIp, remotePort);
+                _udpClient.Send(data, data.Length, endpoint);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Neplatná IP adresa.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Odeslání selhalo: {ex.Message}");
+            }
+        }
+    }
+}
